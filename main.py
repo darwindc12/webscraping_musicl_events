4dd8346bd04ce68ae73b5ccc2db9df0cc5c27a55
darwin <darwindelacruz0012@gmail.com>
2023-01-19 16:54:16 +0800

    Implement OOP programming paradigm

--- main.py
+++ main.py
@@ -7,63 +7,71 @@ import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
-connection = sqlite3.connect("data.db")
-cursor = connection.cursor()
 
-def scrape(url):
-    request = requests.get(url)
-    source = request.text
-    return source
-
-
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Event:
+    def scrape(self, url):
+        request = requests.get(url)
+        source = request.text
+        return source
 
 
-def send_email(message):
-    host = 'smtp.gmail.com'
-    port = 465
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
-    username = "cpythonista@gmail.com"
-    password = "rpoqtvgivcxsgavw"
+class Email:
+    def send_email(self, message):
+        host = 'smtp.gmail.com'
+        port = 465
 
-    receiver = "cpythonista@gmail.com"
-    context = ssl.create_default_context()
+        username = "cpythonista@gmail.com"
+        password = "rpoqtvgivcxsgavw"
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent")
+        receiver = "cpythonista@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent")
+
+class Database:
+
+    def __init__(self, filepath):
+        self.connection = sqlite3.connect(filepath)
+
+    def store(self, extracted_local):
+        row = extracted_local.split(",")
+        row = [word.strip() for word in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO event values(?,?,?)", row)
+        self.connection.commit()
 
 
-def store(extracted_local):
-    row = extracted_local.split(",")
-    row = [word.strip() for word in row]
-    cursor.execute("INSERT INTO event values(?,?,?)", row)
-    connection.commit()
-
-
-def read(extracted_local):
-    row = extracted_local.split(",")
-    row = [word.strip() for word in row]
-    band, city, date = row
-    cursor.execute("SELECT * FROM event WHERE band=? AND city = ? AND date = ?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def read(self, extracted_local):
+        row = extracted_local.split(",")
+        row = [word.strip() for word in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM event WHERE band=? AND city = ? AND date = ?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        result = scrape(URL)
-        extracted = extract(result)
+        event = Event()
+        result = event.scrape(URL)
+        extracted = event.extract(result)
         print(extracted)
         if extracted != "No upcoming tours":
-            rows = read(extracted)
+            database = Database(filepath="data.db")
+            rows = database.read(extracted)
             if not rows:
-                store(extracted)
-                send_email(message="Hey! new event was found")
+                database.store(extracted)
+                email = Email()
+                email.send_email(message="Hey! new event was found")
         time.sleep(1)
 
